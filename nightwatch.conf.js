05ae706d263a3c64d891839ca80322456b6fd018
prateekLambda <prateeks@lambdatest.com>
2022-04-08 16:52:50 +0530

    ng values

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -10,6 +10,8 @@ module.exports = (function (settings) {
 
 
   console.log(settings["test_settings"]["default"]["username"]);
+  console.log(settings["test_settings"]["default"]["username"]);
+
 
   if (process.env.LT_USERNAME) {
     settings["test_settings"]["default"]["username"] = process.env.LT_USERNAME;
@@ -18,7 +20,9 @@ module.exports = (function (settings) {
   if (process.env.LT_ACCESS_KEY) {
     settings["test_settings"]["default"]["access_key"] = process.env.LT_ACCESS_KEY;
   }
-
+  if (process.env.HYPEREXECUTE_PLATFORM) {
+    settings["test_settings"]["default"]["platformName"] = process.env.HYPEREXECUTE_PLATFORM;
+  }
   if (process.env.SELENIUM_HOST) {
     settings.selenium.host = process.env.SELENIUM_HOST;
   }
